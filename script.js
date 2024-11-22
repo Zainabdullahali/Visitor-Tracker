4c2a38066cfb56c64d08b5379168d8ad19ba3227
Zain Abdullah Ali <97894410+Zainabdullahali@users.noreply.github.com>
2024-11-22 21:58:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let countEl=document.getElementById("count-el");
+let saveEl=document.getElementById("save-el");
+let count=0;
+function increment(){
+    count=count+1;
+    countEl.textContent=count;
+}
+function save(){
+    let entries=count+" - ";
+    saveEl.textContent+=entries;
+    count=0;
+    countEl.textContent=count;
+}
